c5f1e318ac36d67d780966c1859265e0bee4b1dd
knechyporenko <ymo4ka.k@gmail.com>
2019-05-30 18:46:42 +0300

    Chat Page main layout

---
+++ newbase/js/chat.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    
+    let search_icon = document.getElementById('search_icon'),
+        search_block = document.getElementById('search_block');
+
+    search_icon.addEventListener('click', function() {
+        search_block.classList.add('active');
+    });
+
+    // btn_nav__close.addEventListener('click', function() {
+    //     nav_panel.classList.remove('header__nav--shown');
+    // });
+
+
+});
