25a69913e89b43e544fa40d8a7a82b33cf5d7013
knechyporenko <ymo4ka.k@gmail.com>
2019-05-31 17:45:27 +0300

    Chat Page: behaviour

--- newbase/js/chat.js
+++ newbase/js/chat.js
@@ -1,16 +1,65 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
+ 
+    let search_icons = document.querySelectorAll('.icon--search'),
+        close_search_icons = document.querySelectorAll('.icon--close');
+    var chat_items = document.querySelectorAll('.chat-item');
 
-    
-    let search_icon = document.getElementById('search_icon'),
-        search_block = document.getElementById('search_block'),
-        close_search_icon = document.getElementById('close_search_icon');
+ 
+    // close_search_icon.addEventListener('click', function() {
+    //     search_block.classList.remove('active');
+    // });
 
-    search_icon.addEventListener('click', function() {
-        search_block.classList.add('active');
+    Array.from(search_icons).forEach(item => {
+        item.addEventListener('click', function(event) {
+            let search_block = item.parentNode;
+            search_block.classList.add('active');
+        });
     });
 
-    close_search_icon.addEventListener('click', function() {
-        search_block.classList.remove('active');
+    Array.from(close_search_icons).forEach(item => {
+        item.addEventListener('click', function(event) {
+            let search_block = item.parentNode;
+            search_block.classList.remove('active');
+        });
+    });
+    
+
+    Array.from(chat_items).forEach(item => {
+        item.addEventListener('click', function(event) {
+            Array.from(chat_items).forEach(item => {
+                item.classList.remove('active');
+            });
+            if (item.classList.contains('collapsed'))
+                item.classList.add('active');
+            else   
+                item.classList.remove('active');
+        });
+    });
+
+
+    let chat_first = document.querySelector(".card-animated");
+    let chat_item_first = document.querySelector(".chat-item");
+
+    toggleChatView(chat_first, chat_item_first);
+
+    window.addEventListener('resize', () => { 
+        toggleChatView(chat_first, chat_item_first);
+        // console.log(document.querySelector(".show"));
     });
 
 });
+
+function toggleChatView(chat_first, chat_item_first) {
+    let chat_shown = document.querySelector(".show");
+    
+    if (window.innerWidth > 767) {
+        if (chat_shown == null) {
+            chat_first.classList.add("show");
+            chat_item_first.classList.remove("collapsed");
+            chat_item_first.classList.add("active");
+        }     
+    } else {
+        // if (chat_shown !== null)
+        //     chat_shown.classList.remove("show");
+    }
+}
