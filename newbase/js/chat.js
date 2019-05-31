084ff423e43ebd5b646ee6242f3c1b1b6fc29d95
knechyporenko <ymo4ka.k@gmail.com>
2019-05-31 15:13:03 +0300

    Chat Page part 2

--- newbase/js/chat.js
+++ newbase/js/chat.js
@@ -2,15 +2,15 @@
 
     
     let search_icon = document.getElementById('search_icon'),
-        search_block = document.getElementById('search_block');
+        search_block = document.getElementById('search_block'),
+        close_search_icon = document.getElementById('close_search_icon');
 
     search_icon.addEventListener('click', function() {
         search_block.classList.add('active');
     });
 
-    // btn_nav__close.addEventListener('click', function() {
-    //     nav_panel.classList.remove('header__nav--shown');
-    // });
-
+    close_search_icon.addEventListener('click', function() {
+        search_block.classList.remove('active');
+    });
 
 });
