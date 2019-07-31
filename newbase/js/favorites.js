01b4a9024a09e8fe521955ac039e3403925e9814
knechyporenko <ymo4ka.k@gmail.com>
2019-07-31 16:33:50 +0300

    Customer page: favorites (list)

---
+++ newbase/js/favorites.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    /* ==================== [Add to Favorite] ===================== */
+
+    let btn_favorite = document.getElementsByClassName('item__icon-favorite');
+
+    Array.from(btn_favorite).forEach(function(element) {
+        element.addEventListener('click', function () {
+           this.classList.toggle('item__icon-favorite--added');
+        });
+    });
+    
+    /* ==================== [Swiper] ===================== */
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination'
+        }
+    });
+    
+});
+
