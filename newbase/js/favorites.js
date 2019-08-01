91ccb37508313030429d094fb44f2215b8c34bfe
knechyporenko <ymo4ka.k@gmail.com>
2019-08-01 13:06:02 +0300

    Customer page: favorites (remove-modal)

--- newbase/js/favorites.js
+++ newbase/js/favorites.js
@@ -1,14 +1,29 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
-    /* ==================== [Add to Favorite] ===================== */
+    
 
     let btn_favorite = document.getElementsByClassName('item__icon-favorite');
+    let btn_remove_favorite = document.getElementById('btn_remove_favorite');
+    let btn_unremove_favorite = document.getElementById('btn_unremove_favorite');
+    let title_favorite = document.getElementById('title_favorite');
+    
+    /* ==================== [Put Item Data to remove-modal] ===================== */
 
     Array.from(btn_favorite).forEach(function(element) {
         element.addEventListener('click', function () {
-           this.classList.toggle('item__icon-favorite--added');
+            title_favorite.innerHTML = element.dataset.item;
+            btn_remove_favorite.dataset.itemId = element.dataset.itemId;
+            btn_unremove_favorite.dataset.itemId = element.dataset.itemId;
         });
     });
+
+    /* ==================== [Remove from Favorite] ===================== */
+
+    btn_remove_favorite.addEventListener('click', function (event) {
+        let item = document.querySelector(`[data-item-id='${event.target.dataset.itemId}']`);
+        item.classList.remove('item__icon-favorite--added');
+    });
+
     
     /* ==================== [Swiper] ===================== */
 
